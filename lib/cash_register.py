ae1173f297d356951eb5f849399472ce5e59d371
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-06-10 10:22:01 +0300

    feat: add cash register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -2,11 +2,16 @@
 cash = float(input("Enter the amount you wanna deposit: "))
 class CashRegister:
   cash = 0
-  
+  total_amount = 0
+  total_items = []
+  previous_transactions = []
+
   def __init__(self, discount, total, items, previous_transactions):
     self.discount = discount
     self.total = total
     self.items = items
     self.previous_transactions = previous_transactions
 
-  
+  @property
+  def discount(self):
+    return self._discount
